76209c346ecc6e6f3c60977e1519fc327f8191c9
github username <github email address>
2019-07-15 17:32:36 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
+require_relative 'config/environment'
+require 'date'
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		erb :hello
 	end
 
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
 
 end
