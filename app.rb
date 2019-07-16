2ee64c323d7f14a3224158541cf16eec36cadbcb
evancarlsen <evan@evancarlsen.com>
2019-07-16 10:50:21 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
-	end
+    get '/' do
+        status 200
+        erb :index
+    end
+    
+    get '/hello' do
+        status 200
+        erb :hello
+    end
 
+    get '/goodbye' do
+        status 200
+        erb :goodbye
+    end
 
+    get '/date' do
+        status 200
+        erb :date
+    end
 end
